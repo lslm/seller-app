1f0d18af737bdd4c090ae939bc20719f4746f69b
Lucas Santos <lucas.santos.silva2011@gmail.com>
2022-05-03 19:50:31 -0300

    Criada opção para cadastrar produto

--- src/main/java/SellerApp.java
+++ src/main/java/SellerApp.java
@@ -1,5 +1,49 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import models.Product;
+
 public class SellerApp {
+    static List<Product> productsDb = new ArrayList<>();
+
     public static void main(String... args) {
         System.out.println("Boas vindas ao Seller App");
+
+        int option;
+
+        do {
+            System.out.println("------- MENU PRINCIPAL --------");
+            System.out.println("1 - Cadastrar um novo produto");
+            System.out.println("0 - Sair do programa");
+
+            System.out.print("Escolha uma operação: ");
+            Scanner scanner = new Scanner(System.in);
+            option = scanner.nextInt();
+
+            process(option);
+        } while (option != 0);
+    }
+
+    static void process(int option) {
+        switch (option) {
+            case 1: {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Informe a descrição do produto: ");
+                String description = scanner.nextLine();
+
+                System.out.print("Informe um ID do produto: ");
+                String id = scanner.nextLine();
+
+                System.out.print("Informe o preço: ");
+                double price = scanner.nextDouble();
+
+                Product newProduct = new Product(id, description, price);
+                productsDb.add(newProduct);
+
+                break;
+            }
+        }
     }
 }
---
+++ src/main/java/models/Product.java
@@ -0,0 +1,37 @@
+package models;
+
+public class Product {
+    private String id;
+    private String description;
+    private double price;
+
+    public Product(String id, String description, double price) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
